1502e3b100d47a82dd2f0f9a451c619d84bdb8fe
Allen Cruiz <cruizallen2@gmail.com>
2024-12-22 15:58:02 +0800

    final push

--- app/Http/Middleware/ContentSecurityPolicy.php
+++ app/Http/Middleware/ContentSecurityPolicy.php
@@ -10,7 +10,7 @@ class ContentSecurityPolicy
     {
         $response = $next($request);
 
-        $csp = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.bunny.net https://ecoswapx.test https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.bunny.net https://ecoswapx.test; img-src 'self' https://i.imgur.com https://ui-avatars.com https://www.fillster.com data:; font-src 'self' https://fonts.bunny.net;";
+        $csp = "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.bunny.net https://ecoswapx.test https://www.google.com https://www.gstatic.com; style-src 'self' 'unsafe-inline' 'unsafe-eval' https://fonts.bunny.net https://ecoswapx.test; img-src 'self' http://ecoswapx.test https://i.imgur.com https://ui-avatars.com https://www.fillster.com data:; font-src 'self' https://fonts.bunny.net;";
 
         $csp .= " connect-src 'self' https://ecoswapx.test;";
 
